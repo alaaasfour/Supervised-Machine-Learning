248654c1f4a7542e99fffc7902c45053634b8bf4
Alaa Asfour <alaa.asfour94@outlook.com>
2024-08-07 16:01:40 -0400

    Compute the Gradient Descent for Linear Regression

--- Gradient_Descent_for_Linear_Regression.py
+++ Gradient_Descent_for_Linear_Regression.py
@@ -22,3 +22,35 @@ def compute_cost(x, y, w, b):
     total_cost = 1 / (2 * m) * cost
     return total_cost
 
+
+"""
+Compute the Gradient Descent for Linear Regression
+
+Arguments:
+    x (ndarray (m,)): input data, m examples
+    y (ndarray (m,)): target values
+    w, b (scalar): model parameters
+
+Returns:
+    dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+    dj_db (scalar): The gradient of the cost w.r.t. the parameters b
+"""
+def compute_gradient(x, y, w, b):
+    # Number of training examples
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        dj_dw_i = (f_wb - y[i]) * x[i]
+        dj_db_i = (f_wb - y[i])
+        dj_dw += dj_dw_i
+        dj_db += dj_db_i
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_dw, dj_db
+
+plt_gradients(x_train, y_train, compute_cost, compute_gradient)
+plt.show()
