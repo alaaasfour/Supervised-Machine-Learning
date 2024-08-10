3d70bd99f85677ee89be055ad441190fae3aac03
Alaa Asfour <alaa.asfour94@outlook.com>
2024-08-10 14:32:47 -0400

    Create Feature_Scaling_and_Learning_Rate File

---
+++ Feature_Scaling_and_Learning_Rate.py
@@ -0,0 +1,13 @@
+"""
+In this lab, we will achieve the following items:
+    - Utilize the multiple variables routines developed in the previous lab
+    - Run Gradient Descent on a data set with multiple features
+    - Explore the impact of the learning rate alpha on gradient descent
+    - Improve performance of gradient descent by feature scaling using z-score normalization
+"""
+
+# Packages
+import numpy as np
+import matplotlib.pyplot as plt
+np.set_printoptions(precision=2)
+plt.style.use('./deeplearning.mplstyle')
