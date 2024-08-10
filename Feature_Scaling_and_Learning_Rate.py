c75ff1c8d145873c8b781aca082e12a38f6a6e42
Alaa Asfour <alaa.asfour94@outlook.com>
2024-08-10 14:54:54 -0400

    Add the houses data

--- Feature_Scaling_and_Learning_Rate.py
+++ Feature_Scaling_and_Learning_Rate.py
@@ -10,5 +10,26 @@ In this lab, we will achieve the following items:
 import numpy as np
 import matplotlib.pyplot as plt
 np.set_printoptions(precision=2)
-from lab_utils_multi 
+from lab_utils_multi import load_house_data, run_gradient_descent, norm_plot, plt_equal_scale, plot_cost_i_w
+from lab_utils_common import dlc
 plt.style.use('./deeplearning.mplstyle')
+
+"""
+The dataset:
+We will use the motivating example of housing price prediction. The training data set contains many examples with 4 features 
+(size, bedrooms, floors and age). In this lab, the Size feature is in sqft. This data set is large.
+We would like to build a linear regression model using these values so we can then predict the price for 
+other houses - say, a house with 1200 sqft, 3 bedrooms, 1 floor, 40 years old.
+"""
+
+# Load the dataset
+X_train, y_train = load_house_data()
+X_features = ['size(sqft)', 'bedrooms', 'floors', 'age']
+
+# Let's view the dataset and its features by plotting each feature versus price
+fig, ax=plt.subplots(1, 4, figsize=(12, 3), sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X_train[:,i], y_train)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("Price (1000's)")
+plt.show()
