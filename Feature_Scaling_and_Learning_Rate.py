c1b11378129bdafa3578b3d762f59523a898d601
Alaa Asfour <alaa.asfour94@outlook.com>
2024-08-10 23:09:38 -0400

    Data Normalization and Feature Scaling

--- Feature_Scaling_and_Learning_Rate.py
+++ Feature_Scaling_and_Learning_Rate.py
@@ -49,3 +49,73 @@ _,_,hist = run_gradient_descent(X_train, y_train, 10, alpha = 1e-7)
 plot_cost_i_w(X_train, y_train, hist)
 
 
+"""
+Feature Scaling 
+z-score normalization: After z-score normalization, all features will have a mean of 0 and a standard deviation of 1.
+
+Args:
+    X (ndarray (m,n))     : input data, m examples, n features
+      
+Returns:
+    X_norm (ndarray (m,n)): input normalized by column
+    mu (ndarray (n,))     : mean of each feature
+    sigma (ndarray (n,))  : standard deviation of each feature
+"""
+def zscore_normalize_feature(X):
+    # Find the mean of each column/feature
+    mu = np.mean(X, axis=0)
+
+    # Find the standard deviation of each column/feature
+    sigma = np.std(X, axis=0)
+
+    # Element-wise, subtract mu for that column from each example, divide by std for that column
+    X_norm = (X - mu) / sigma
+
+    return (X_norm, mu, sigma)
+
+mu     = np.mean(X_train,axis=0)
+sigma  = np.std(X_train,axis=0)
+X_mean = (X_train - mu)
+X_norm = (X_train - mu)/sigma
+
+fig,ax=plt.subplots(1, 3, figsize=(12, 3))
+ax[0].scatter(X_train[:,0], X_train[:,3])
+ax[0].set_xlabel(X_features[0]); ax[0].set_ylabel(X_features[3]);
+ax[0].set_title("unnormalized")
+ax[0].axis('equal')
+
+ax[1].scatter(X_mean[:,0], X_mean[:,3])
+ax[1].set_xlabel(X_features[0]); ax[0].set_ylabel(X_features[3]);
+ax[1].set_title(r"X - $\mu$")
+ax[1].axis('equal')
+
+ax[2].scatter(X_norm[:,0], X_norm[:,3])
+ax[2].set_xlabel(X_features[0]); ax[0].set_ylabel(X_features[3]);
+ax[2].set_title(r"Z-score normalized")
+ax[2].axis('equal')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+fig.suptitle("distribution of features before, during, after normalization")
+plt.show()
+
+# Now, let's normalize the data and compare it to the original data
+# Normalize the original feature
+X_norm, X_mu, X_sigma = zscore_normalize_feature(X_train)
+print(f"X_mu = {X_mu}, \nX_sigma = {X_sigma}")
+print(f"Peak to Peak range by column in Raw        X:{np.ptp(X_train,axis=0)}")
+print(f"Peak to Peak range by column in Normalized X:{np.ptp(X_norm,axis=0)}")
+
+fig,ax=plt.subplots(1, 4, figsize=(12, 3))
+for i in range(len(ax)):
+    norm_plot(ax[i],X_train[:,i],)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("count");
+fig.suptitle("distribution of features before normalization")
+plt.show()
+fig,ax=plt.subplots(1,4,figsize=(12,3))
+for i in range(len(ax)):
+    norm_plot(ax[i],X_norm[:,i],)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("count");
+fig.suptitle("distribution of features after normalization")
+
+plt.show()
