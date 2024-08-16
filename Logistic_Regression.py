400e78b62cd87029f7d7dcf704dab03562a0c2d5
Alaa Asfour <alaa.asfour94@outlook.com>
2024-08-16 18:09:31 -0400

    Ex 2: Cost Function for Logistic Regression

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -63,3 +63,55 @@ print ("sigmoid([ -1, 0, 1, 2]) = " + str(sigmoid(np.array([-1, 0, 1, 2]))))
 sigmoid_test(sigmoid)
 print("========================================")
 
+"""
+Exercise 2: Cost Function for Logistic Regression
+We also need to implement the cost function to build the logistic regression.
+
+Args:
+    X : (ndarray Shape (m,n)) data, m examples by n features
+    y : (ndarray Shape (m,))  target value 
+    w : (ndarray Shape (n,))  values of parameters of the model      
+    b : (scalar)              value of bias parameter of the model
+    *argv : unused, for compatibility with regularized version below
+
+Returns:
+    total_cost : (scalar) cost 
+"""
+
+def compute_cost(X, y, w, b, *argv):
+    m, n = X.shape
+    loss_sum = 0
+    for i in range(m):
+        z_wb = 0
+        for j in range(n):
+            z_wb_ij = w[j] * X[i][j]
+            z_wb += z_wb_ij
+        z_wb += b
+
+        f_wb = sigmoid(z_wb)
+        loss = -y[i] * np.log(f_wb) - (1 - y[i]) * np.log(1 - f_wb)
+        loss_sum += loss
+
+    total_cost = loss_sum / m
+    return total_cost
+
+print("Exercise 2: Cost Function for Logistic Regression")
+print("==========")
+m, n = X_train.shape
+
+# Compute and display cost with w and b initialized to zeros
+initial_w = np.zeros(n)
+initial_b = 0.
+cost = compute_cost(X_train, y_train, initial_w, initial_b)
+print('Cost at initial w and b (zeros): {:.3f}'.format(cost))
+
+# Compute and display cost with non-zero w and b
+test_w = np.array([0.2, 0.2])
+test_b = -24.
+cost = compute_cost(X_train, y_train, test_w, test_b)
+
+print('Cost at test w and b (non-zeros): {:.3f}'.format(cost))
+
+# UNIT TESTS
+compute_cost_test(compute_cost)
+print("========================================")
